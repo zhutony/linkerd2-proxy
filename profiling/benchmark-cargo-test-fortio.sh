39142b77f0292a64fb7082d40304b5b0f98c64ce
Eliza Weisman <eliza@buoyant.io>
2020-01-14 13:28:41 -0800

    add stddev to full
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- profiling/benchmark-cargo-test-fortio.sh
+++ profiling/benchmark-cargo-test-fortio.sh
@@ -39,7 +39,7 @@ trap '{ killall iperf fortio >& /dev/null; }' EXIT
 # Summary table header
 echo "Test, target req/s, req len, branch, p999 latency (ms), GBit/s" > "summary.$BRANCH_NAME.$ID.txt"
 
-echo "Test,target req/s,req len,branch,run,p50 latency (ms),p90 latency (ms),p99 latency (ms),p999 latency (ms),GBit/s" > "full.$BRANCH_NAME.$ID.csv"
+echo "Test,target req/s,req len,branch,run,p50 latency (ms),p90 latency (ms),p99 latency (ms),p999 latency (ms),stddev,GBit/s" > "full.$BRANCH_NAME.$ID.csv"
 
 single_benchmark_run () {
   # run benchmark utilities in background, only proxy runs in foreground
@@ -95,7 +95,8 @@ single_benchmark_run () {
           P90=$(jq '.DurationHistogram.Percentiles | .[2] |.Value' < "$NAME-$r-rps.$ID.json")
           P99=$(jq '.DurationHistogram.Percentiles | .[3] |.Value' < "$NAME-$r-rps.$ID.json")
           P999=$(jq '.DurationHistogram.Percentiles | .[4] |.Value' < "$NAME-$r-rps.$ID.json")
-          echo "$MODE $DIRECTION,$r,$l,$RUN_NAME,$i,$P50,$P90,$P99,$P999,0" >> "full.$BRANCH_NAME.$ID.csv"
+          STDDEV=$(jq '.DurationHistogram.Std')
+          echo "$MODE $DIRECTION,$r,$l,$RUN_NAME,$i,$P50,$P90,$P99,$P999,$STDDEV,0" >> "full.$BRANCH_NAME.$ID.csv"
         done
         echo "$MODE $DIRECTION, $r, $l, $RUN_NAME, $S, 0" >> "summary.$BRANCH_NAME.$ID.txt"
       done
